33a7f43925d7d02959d0088ff31a99319f6fb567
Tern Yuka Følbæk <tfolbaek@gmail.com>
2025-05-04 12:00:31 +0200

    Update git config user

--- obj/Debug/net9.0/ASP.NET MVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASP.NET MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.NET MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97104dcaee80feda15d69f2462524c5883325e88")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.NET MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.NET MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
